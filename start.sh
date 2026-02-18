d7628fae456295cc90708880501c2366f1ff409c
Anton Stanoev <121201900+astanoev17@users.noreply.github.com>
2026-02-18 22:02:09 +0200

    Fix execution command for Streamlit in start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# Start API on 8080
 uvicorn api_server:app --host 0.0.0.0 --port 8080 &
 
-# Start Streamlit on 8501 (your UI)
 exec streamlit run main.py --server.address=0.0.0.0 --server.port=8501
